0940f3a7ee8d2f382f2681d30843e805368297d7
aquariusjr10 <deepakjeganathan@gmail.com>
2021-02-05 12:35:06 +0000

    Merge tag 'v3.18.125' of https://git.kernel.org/pub/scm/linux/kernel/…
    …git/stable/linux into upstream

--- drivers/scsi/scsi_pm.c
+++ drivers/scsi/scsi_pm.c
@@ -262,9 +262,10 @@ static int sdev_runtime_suspend(struct device *dev)
 		err = blk_pre_runtime_suspend(sdev->request_queue);
 		if (err)
 			return err;
+
 		err = pm->runtime_suspend(dev);
-		blk_post_runtime_suspend(sdev->request_queue, err);
-	}
+	blk_post_runtime_suspend(sdev->request_queue, err);
+
 	return err;
 }
 
@@ -293,8 +294,8 @@ static int sdev_runtime_resume(struct device *dev)
 	if (pm && pm->runtime_resume) {
 		blk_pre_runtime_resume(sdev->request_queue);
 		err = pm->runtime_resume(dev);
-		blk_post_runtime_resume(sdev->request_queue, err);
-	}
+	blk_post_runtime_resume(sdev->request_queue, err);
+
 	return err;
 }
 
